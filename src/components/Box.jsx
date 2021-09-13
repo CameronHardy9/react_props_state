8f28d507290406e3eb8490a26ad62b2854cbef08
Cameron Hardy <camhardy99@gmail.com>
2021-09-13 14:52:41 -0500

    exercise done

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -4,14 +4,16 @@ const Box = (props) => {
   return (
     <div
       style={{
-        width: "180px",
-        height: "180px",
+        width: "140px",
+        height: "140px",
         backgroundColor: props.color,
         border: "1px solid black",
         display: "inline-block",
       }}
+      onClick={props.handleClick}
+      id={props.id}
     >
-      <span>{/* Add rgb value from props */}</span>
+      <span>{props.color}</span>
     </div>
   );
 };
